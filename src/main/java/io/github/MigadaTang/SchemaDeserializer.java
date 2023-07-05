d541c3a5978d5c88c4746b2a6b070319d83e613c
David Bull <david.bull22@imperial.ac.uk>
2023-07-05 22:33:44 +0100

    Changed visibility of SchemaDeserializer to public so that it can be used in dependent package.

--- src/main/java/io/github/MigadaTang/SchemaDeserializer.java
+++ src/main/java/io/github/MigadaTang/SchemaDeserializer.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-class SchemaDeserializer extends StdDeserializer<Schema> {
+public class SchemaDeserializer extends StdDeserializer<Schema> {
 
     private Map<String, Entity> entityNameMap = new HashMap<>();
     private Map<String, Relationship> relationshipNameMap = new HashMap<>();
